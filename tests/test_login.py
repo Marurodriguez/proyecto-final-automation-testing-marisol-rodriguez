7a6e279843a16206af3f6ba38d71d5b5469a7bbf
Marisol Rodríguez <rodriguezsmarisol@gmail.com>
2025-12-15 11:21:46 -0300

    Agrego escenario negativo de login inválido

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,3 +12,16 @@ def test_login_valido():
     assert "inventory" in driver.current_url
 
     driver.quit()
+
+
+def test_login_invalido():
+    driver = get_driver()
+    login_page = LoginPage(driver)
+
+    login_page.open()
+    login_page.login("locked_out_user", "wrong_password")
+
+    error_message = login_page.get_error_message()
+    assert "Epic sadface" in error_message
+
+    driver.quit()
